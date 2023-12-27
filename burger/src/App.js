7ef63b8a697172ce8542fefe4520d97e59891cc6
FurkanAlar <122054850+FurkanAlar@users.noreply.github.com>
2023-12-27 16:51:45 +0300

    first commit

---
+++ burger/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
